60159e817b112d5e7961eeb6fa9137e8be902cd7
Tim Kleinelsen <44807106+TimKlsen@users.noreply.github.com>
2025-05-09 09:18:46 +0200

    Projektdateien hinzufügen.

---
+++ MyHompage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyHompage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
